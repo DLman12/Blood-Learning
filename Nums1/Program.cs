eff91ae686c0383863dc9b57ecde0bb01a9cf883
mihail <mihail.m1221@mail.ru>
2025-12-02 00:12:36 +0300

    Test2

---
+++ Nums1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Nums1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var nums = new Nums();
+        var result = nums.GetOddNumbers([1, 2, 3, 4, 5]);
+        foreach (var i in result)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
