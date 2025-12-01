2eff2378d0aa5ff809b3ef5ffdcae6a1fbb2e794
mihail <mihail.m1221@mail.ru>
2025-12-02 00:51:03 +0300

    TestProject

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Nums1;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace TestProject1;
+
+public class Tests
+{
+
+    private readonly Nums _nums = new();
+
+    [Fact]
+    public void Test1()
+    {
+        var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        var expected = new List<int> { 1, 3, 5, 7, 9 };
+
+        var result = _nums.GetOddNumbers(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+}
