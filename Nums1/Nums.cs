2fd5665936e0ca0ca7db6d77ee5121b9d5d24063
mihail <mihail.m1221@mail.ru>
2025-12-02 00:10:58 +0300

    Test

---
+++ Nums1/Nums.cs
@@ -0,0 +1,13 @@
+﻿namespace Nums1;
+
+public class Nums
+{
+    public IEnumerable<int> GetOddNumbers(ICollection<int>? numbers)
+    {
+        if (numbers == null || numbers.Count == 0)
+        {
+            return [];
+        }
+        return numbers.Where(x => x % 2 != 0);
+    } 
+}
