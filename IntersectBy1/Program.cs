4e82224e3602ef95cea941c3b6ec9806adf357d3
mihail <mihail.m1221@mail.ru>
2025-12-11 01:55:37 +0300

    н

--- IntersectBy1/Program.cs
+++ IntersectBy1/Program.cs
@@ -4,26 +4,28 @@ public class Program4
 {
     public static void Main(string[] args)
     {
-        
- 
+
+
         var users1 = new List<IntersectBy.User>
         {
             new() { Id = Guid.NewGuid(), Age = 18, Name = "John Doe" },
             new() { Id = Guid.NewGuid(), Age = 21, Name = "Jane Doe" },
             new() { Id = Guid.NewGuid(), Age = 18, Name = "John Double" }
         };
-        
+
         var users2 = new List<IntersectBy.User>
         {
             new() { Id = Guid.NewGuid(), Age = 19, Name = "John Doe" },
             new() { Id = Guid.NewGuid(), Age = 18, Name = "Jane Den" },
             new() { Id = Guid.NewGuid(), Age = 17, Name = "John Two" }
         };
-        
+
+
         var result = new IntersectBy().FindUsersWithSameName(users1, users2);
-        foreach (var user  in result)
-        {   
-            Console.WriteLine($"{user.Id} - {user.Age} - {user.Name}");
+        foreach (var user in result) 
+        {
+            Console.WriteLine(user);
         }
+        
     }
 }
