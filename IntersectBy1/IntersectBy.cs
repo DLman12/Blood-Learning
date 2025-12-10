64145e9553f403bdcd9f61a78f45633f0e69b502
mihail <mihail.m1221@mail.ru>
2025-12-11 00:48:30 +0300

    Intersect

---
+++ IntersectBy1/IntersectBy.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassLibrary1;
+
+public class IntersectBy
+{
+    public sealed record User
+    {
+        public required Guid Id { get; init; }
+        public required int Age { get; init; }
+        public required string Name { get; init; }
+    }
+
+    public ICollection<User> FindUsersWithSameName(
+        ICollection<User> firstGroup,
+        ICollection<User> secondGroup)
+    {
+        return firstGroup.IntersectBy(secondGroup.Select(x=>x.Name),x=>x.Name).ToList();
+    }
+}
---
+++ IntersectBy1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassLibrary1;
+
+public class Program4
+{
+    public static void Main(string[] args)
+    {
+        
+ 
+        var users1 = new List<IntersectBy.User>
+        {
+            new() { Id = Guid.NewGuid(), Age = 18, Name = "John Doe" },
+            new() { Id = Guid.NewGuid(), Age = 21, Name = "Jane Doe" },
+            new() { Id = Guid.NewGuid(), Age = 18, Name = "John Double" }
+        };
+        
+        var users2 = new List<IntersectBy.User>
+        {
+            new() { Id = Guid.NewGuid(), Age = 19, Name = "John Doe" },
+            new() { Id = Guid.NewGuid(), Age = 18, Name = "Jane Den" },
+            new() { Id = Guid.NewGuid(), Age = 17, Name = "John Two" }
+        };
+        
+        var result = new IntersectBy().FindUsersWithSameName;
+        foreach(var user in users1)
+        {   
+            Console.WriteLine($"{user.Id} - {user.Age} - {user.Name}");
+        }
+    }
+}
